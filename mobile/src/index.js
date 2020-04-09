2e31eb6c1a06826ae35b43396453d19043db5dfd
Wellington Reis <wellington.costa.97@gmail.com>
2020-04-09 18:50:16 -0300

    Only this commit

---
+++ mobile/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings(['Unrecognized WebSocket']);
+
+import Routes from './routes.js';
+import Routes from './routes.js';
+
+export default function App() {
+	return <Routes />;
+}
